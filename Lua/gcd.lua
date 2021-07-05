0df548c82fdc4addaf1bd8a27980e21b675f4163
st127 <tswuyin_st127@163.com>
2021-07-05 16:58:11 +0800

    gcd() in Lua

---
+++ Lua/gcd.lua
@@ -0,0 +1,25 @@
+--[[
+	code
+	File:/Lua/gcd.lua
+	Date:2021.07.03
+	By MIT License.
+	Copyright(C) 2021 Suote127.All rights reserved.
+]]
+
+local math	= require("math");
+local os	= require("os");
+
+local gcd;
+gcd		= function(a,b)
+	return a % b == 0 and b or gcd(b,a%b);
+end
+
+math.randomseed(os.time());
+
+for i = 1,128
+do
+	local a,b = math.random(1,math.maxinteger),math.random(1,math.maxinteger);
+	print(string.format("gcd(%d,%d) = %d",a,b,gcd(a,b)));
+end
+
+return 0;
